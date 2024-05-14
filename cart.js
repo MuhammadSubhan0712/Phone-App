ce9bd57ed211a16814c37a6bd60e08d08edfd268
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-05-15 01:03:23 +0500

    Assigned

--- cart.js
+++ cart.js
@@ -11,7 +11,7 @@ function renderItems (){
             console.log(cartItems[i]);
             div.innerHTML += `
             <div class="bg-secondary  border border-light rounded m-5 p-3">
-            <img> ${cartItems[i]} </img>
+            <img> ${cartItems[i].img} </img>
             <h1>Model: ${cartItems[i].brand + ' ' + cartItems[i].model}</h1>
             <h1>Price: ${cartItems[i].price}</h1>
             </div>
