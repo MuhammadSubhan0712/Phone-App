850832d16ae3e3f2e38400aff2d181be34d9e261
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-05-15 01:02:34 +0500

    Assigned

--- cart.js
+++ cart.js
@@ -10,7 +10,7 @@ function renderItems (){
         for(let i = 0; i < cartItems.length; i++){
             console.log(cartItems[i]);
             div.innerHTML += `
-            <div class="border border-light rounded m-5 p-3">
+            <div class="bg-secondary  border border-light rounded m-5 p-3">
             <img> ${cartItems[i]} </img>
             <h1>Model: ${cartItems[i].brand + ' ' + cartItems[i].model}</h1>
             <h1>Price: ${cartItems[i].price}</h1>
@@ -18,9 +18,9 @@ function renderItems (){
             `
         }
     }else{
-        console.log('cart item empty ha maalik')
+        console.log('Cart items are Empty!!!')
         div.innerHTML = `
-        <h3 class="text-center">No Item Found...</h3>
+        <h3 class="text-center">No Items Found...</h3>
         `
     }
 }
